72d39716a231a7ee3eca23f8c9205d8f27bf9df7
Ryan <ryanlee02@foxmail.com>
2024-01-02 01:59:40 +0800

    重构一遍之后清晰了许多, 能过除了reassembler_win以外所有的测试, 但是哪里应该还是有小的逻辑的错误, 需要再排查

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,166 +5,143 @@ using namespace std;
 
 Reassembler::Reassembler()
   : pending_buffer()
-  , receive_window_size( 0 )
-  ,
-  // last_acceptable_bytes(0),
-  last_bytes_received( 0 )
-  , last_ordered_bytes( 0 )
   , _bytes_pending( 0 )
-  , finish_flag( false )
-  , index_of_end( 0 )
+  // , first_unassembled_index( 0 )
+  , first_unassembled_index( 0 )
+  , _EOF_index( 0 )
+  , received_FIN( false )
+  , available_capacity( 0 )
 {}
-
-void Reassembler::auto_push( uint64_t first_index, Writer& output, std::string current_data )
-{
-  // std::cout << "+++Auto-push called with first_index: " << first_index << ", current_data size: " <<
-  // current_data.size() << std::endl;
-
-  cout << "[AUTO PUSH] Start ==> | First index: " << first_index << ", Data size: " << current_data.size() << endl;
-
-  uint64_t pushed_buffer_size = current_data.length();
-  output.push( current_data );
-  // std::cout << "Pushed initial data segment. Size: " << pushed_buffer_size << std::endl;
-  last_ordered_bytes = first_index + pushed_buffer_size;
-  // std::cout << "Last ordered: " << last_ordered_bytes << std::endl;
-
-  uint64_t index = last_ordered_bytes;
-  auto it = pending_buffer.find( index );
-
-  while ( it != pending_buffer.end() ) {
-    // std::cout << "Found pending buffer at index: " << index << ". Pushing its data." << std::endl;
-    output.push( pending_buffer[index] );
-    pushed_buffer_size = pending_buffer[index].length();
-    pending_buffer.erase( index ); //回收内存
-    index += pushed_buffer_size;
-    last_ordered_bytes = index;
-    _bytes_pending -= pushed_buffer_size;
-
-    // std::cout << "Last ordered: " << last_ordered_bytes << ", Pending bytes: " << _bytes_pending << std::endl;
-
-    it = pending_buffer.find( index ); // Update iterator for the next loop iteration
-  }
-
-  if ( finish_flag && last_ordered_bytes == index_of_end ) {
-    output.close();
-  }
-  cout << "[AUTO PUSH] End <==| Last ordered: " << last_ordered_bytes << ", Pending bytes: " << _bytes_pending << endl;
-
-}
 
 void Reassembler::insert( uint64_t first_index, std::string data, bool is_last_substring, Writer& output )
 {
-  // std::cout << "===Insert called with first_index: " << first_index<<" ,data: \""<<data << "\", data size: " <<
-  // data.size() << std::endl;
+  std::cout << "[开始插入] first_index: " << first_index << ", data length: " << data.length()
+            << ", is_last_substring: " << is_last_substring << std::endl;
+  available_capacity = output.available_capacity();
+  std::cout << "[当前窗口大小] available_capacity: " << available_capacity << std::endl;
 
-  if ( first_index < last_ordered_bytes ) {
-    // overlap
-    //  cout<<"overlap"<<endl;
-    uint64_t dif = last_ordered_bytes - first_index;
-    if ( dif >= data.length() ) {
-      //如果这个字符串已经push出去了, 忽略重叠
+  // 全部超出, 忽略
+  if ( first_index > first_unassembled_index + available_capacity ) {
+    std::cout << "[全部超出范围] 忽略插入" << std::endl;
+    return;
+  }
+  // 部分超出, 存每超出的内容
+  else if ( first_index + data.length() > first_unassembled_index + available_capacity ) {
+    uint64_t out_of_range = first_index + data.length() - ( first_unassembled_index + available_capacity );
+    uint64_t n = data.length() - out_of_range;
+    std::cout << "[部分超出范围] 保留未超出部分: " << n << " bytes" << std::endl;
+    data = data.substr( 0, n );
+  }
+  // 没有超出范围, 存全部内容
+  else {
+    if ( is_last_substring ) {
+      std::cout << "[在范围内收到FIN] 标记EOF" << std::endl;
+      received_FIN = true;
+      _EOF_index = first_index + data.length();
+    }
+  }
+
+
+
+  // 处理重叠的情况, 先考虑边界, 再考虑重叠, 窗口部分已经处理过右边界了, 现在只需要处理左边界
+  
+  if (first_index >= first_unassembled_index) {
+    std::cout << "[块完全未被pop出去] 不需要截取" << std::endl;
+  }
+  // 对于部分pop出去的块, 截取未被pop出去的部分
+  else if ( (first_index < first_unassembled_index) 
+        && (first_index+data.length()>first_unassembled_index)) {
+    std::cout << "[块部分pop出去] 截取未被pop出去的部分, ";
+    uint64_t beyond_first_unpoped = first_index+data.length()- first_unassembled_index;
+    data = data.substr( data.length()-beyond_first_unpoped );
+    std::cout << "取后" <<data.length()-beyond_first_unpoped <<"位"<< std::endl;
+    first_index = first_unassembled_index;
+  }
+  // 对于已经pop出去的块, 忽略
+  else {
+    std::cout << "[块已被pop出去] 忽略" << std::endl;
+    return;
+  }
+
+  // 判断是否覆盖了现有的数据, 对于完全重叠的块, 认为是多发了几个包, 忽略, 对于覆盖到其它部分的块, 认为是更新,
+  // 修改它的结构
+  std::cout << "[检查覆盖] 正在检查覆盖情况..." << std::endl;
+
+  auto iter = pending_buffer.begin();  // 使用显式迭代器
+
+  while (iter != pending_buffer.end()) {  // 使用while循环代替for循环
+    auto current = iter++;  // 保存当前元素的迭代器，并预先递增it到下一个元素
+
+    if ((first_index <= current->first) && (first_index + data.length() >= current->first + current->second.length())) {
+        std::cout << "[完全覆盖] 删除旧数据块" << std::endl;
+        _bytes_pending -= current->second.length();
+        pending_buffer.erase(current);  // 删除当前元素，并不需要更新迭代器，因为它已经预先递增了
+    } else if ((first_index <= current->first)
+               && (first_index + data.length() > current->first) //大于左边
+               && (first_index + data.length() < current->first+current->second.length()) //小于右边
+               ) {
+        uint64_t beyond_cut_off = first_index + data.length() - current->first ;
+        std::cout << "[部分覆盖] 截取未被覆盖的部分 退出遍历" << std::endl;
+        data = data.substr(0, data.length()- beyond_cut_off);
+        break;  // 退出循环
+    } else if (first_index + data.length() < current->first) {
+        std::cout << "[没有覆盖] 退出遍历" << std::endl;
+        break;  // 退出循环
+    } else if ((first_index > current->first)
+               && (first_index + data.length() < current->first + current->second.length())) {
+        data = "";
+        std::cout << "[被覆盖] 清除data,退出遍历" << std::endl;
+        break;  // 退出循环
+    }
+  }
+    // 在pending_buffer中存""浪费index的内存, 并没有意义,
+    // 但是''如果出现在first_unassembled_index上, 如果是EOF, 不处理会丢失状态
+  if ( ( data.length() == 0 ) && ( first_index > first_unassembled_index ) ) {
+    std::cout << "[忽略空数据] 不在期望的位置" << std::endl;
+    return;
+  } else if ( ( data.length() == 0 ) && ( first_index == first_unassembled_index ) ) {
+    std::cout << "[收到空数据] 在期望的位置" << std::endl;
+    if ( received_FIN ) {
+      std::cout << "[接收到FIN] 关闭输出" << std::endl;
+      auto it = pending_buffer.find( first_unassembled_index );
+      if( it != pending_buffer.end() ) {
+        output.push( it->second ); // 注意这里应使用迭代器的值
+      }
+      output.close();
       return;
     }
-    //如果没处理完, 就继续处理
-    data = data.substr( dif );
-    first_index = last_ordered_bytes;
-  } else {
-    //检查一下长度, 不要额外覆盖到后边的
-    for ( const auto& pair : pending_buffer ) {
-      // cout << "Checking against buffer element with first index: " << pair.first << " and data: " << pair.second
-      // << endl;
-      //在最后一个的里边
-      if ( first_index <= pair.first ) {
-        // cout<<"first_index + data.length() :"<<first_index + data.length()<<endl;
-        // cout<<"pair.first: "<<pair.first<<endl;
-        // cout<<"first_index + data.length(): "<<first_index + data.length()<<endl;
-        // cout<<" pair.first + pair.second.length(): "<< pair.first + pair.second.length()<<endl;
-        if ( ( first_index + data.length() > pair.first )
-             && ( first_index + data.length() < pair.first + pair.second.length() ) ) {
-          // Partial overlap
-          uint64_t overlapLength = first_index + data.length() - pair.first;
-          // cout << "Partial overlap detected! Overlap length: " << overlapLength << endl;
-          uint64_t newLength = data.length() - overlapLength;
-          // cout << "Original data: \"" << data << "\" | Trimming to new length: " << newLength << endl;
-          data = data.substr( 0, newLength );
-          // cout << "Data after trimming: \"" << data << "\"" << endl;
-          break; // Exiting after the first partial overlap is handled.
-        } else if ( first_index + data.length() >= pair.first + pair.second.length() ) {
-          // Full overlap
-          // cout << "Full overlap detected! Removing entire block with first index: " << pair.first << endl;
-          _bytes_pending -= pair.second.length();
-          pending_buffer.erase( pair.first );
-          // cout << "Removed block. New bytes pending: " << _bytes_pending << endl;
-        } else {
-          // No recognizable overlap
-          // cout << "No overlap or unrecognized overlap pattern with current buffer element." << endl;
-          // cout << "Continuing to next buffer element if any." << endl;
-        }
-      } else {
-        //在外边, 如果被之前的已经包围了的一条
-        if ( pair.first + pair.second.length() >= first_index + data.length() ) {
-          if ( is_last_substring ) {
-            finish_flag = true;
-            index_of_end = first_index + data.length();
-            //如果收到结束信号, 就记录一下, 等数据都收完了, 就结束.
-          }
-
-          return;
-        }
-      }
-    }
-  }
-  // std::cout << "===++after repaired first_index: " << first_index<<" ,data: \""<<data << "\", data size: " <<
-  // data.size() << std::endl;
-
-  if ( is_last_substring ) {
-    finish_flag = true;
-    index_of_end = first_index + data.length();
-    //如果收到结束信号, 就记录一下, 等数据都收完了, 就结束.
   }
 
- 
-  receive_window_size = output.available_capacity();
-  // last_acceptable_bytes = last_ordered_bytes + receive_window_size;
-  last_bytes_received = first_index + data.length();
-  // cout<<"receive window size: "<<receive_window_size<<endl;
-  // std::cout << "Last bytes received: " << last_bytes_received << std::endl;
-  // std::cout << "Last ordered bytes: " << last_ordered_bytes << std::endl;
+  std::cout << "[处理顺序块] 正在处理顺序块..." << std::endl;
+  if ( first_index == first_unassembled_index ) {
+    std::cout << "[顺序块匹配] 发现顺序块, 准备输出数据..." << std::endl;
+    output.push( data );
+    first_unassembled_index += data.length();
 
-  if ( last_bytes_received - last_ordered_bytes > receive_window_size ) {
-    uint64_t n = data.length() - ( ( last_bytes_received - last_ordered_bytes ) - receive_window_size );
-    // std::cout << "Data beyond window size, trimming to " << n << " bytes" << std::endl;
-    if ( n > 0 ) {
-      std::string temp_str = data.substr( 0, n );
-      auto_push( first_index, output, temp_str );
+    // 检查是否构成连续的数据块, 就一直push连续的数据
+    auto it = pending_buffer.find( first_unassembled_index );
+    while ( it != pending_buffer.end() ) {
+      std::cout << "[连续块处理] 输出连续块数据..." << std::endl;
+      output.push( it->second ); // 注意这里应使用迭代器的值
+      _bytes_pending -= it->second.length();
+      first_unassembled_index += it->second.length();
+
+      pending_buffer.erase( it );                          // 回收内存，这里直接使用迭代器
+      it = pending_buffer.find( first_unassembled_index ); // 更新迭代器
     }
-
-  } else { // 如果是在窗口范围内的
-    // 如果乱序块还没形成, 还是头一个, 可直接push
-    if ( last_ordered_bytes == first_index ) {
-      // std::cout << "In order chunk. Pushing directly." << std::endl;
-      auto_push( first_index, output, data );
-    } else {
-      // 来了之后不构成顺序块, 就先存着
-      if ( first_index != last_ordered_bytes + pending_buffer[last_ordered_bytes].length() ) {
-        auto it = pending_buffer.find( first_index );
-        // 如果是不重复的,就插入,
-        if ( it == pending_buffer.end() ) {
-          // std::cout << "Out of order chunk. Buffering." << std::endl;
-          pending_buffer.insert( std::make_pair( first_index, data ) );
-          _bytes_pending += data.length();
-        } else {
-          //如果是重复的, 就修改
-          pending_buffer[first_index] = data;
-        }
-      } else {
-        // 构成了顺序块, 就push出去.
-        // std::cout << "Formed an in-order chunk. Pushing." << std::endl;
-        auto_push( first_index, output, data );
-      }
+    // 如果pop到最后一个了, 就关闭
+    if ( first_unassembled_index == _EOF_index &&received_FIN==true ) {
+      std::cout << "[到达EOF] 最后一个字节已输出，关闭输出流..." << std::endl;
+      output.close();
+    }
+  } else { // 不构成顺序块, 存起来
+    std::cout << "[非顺序块] 存储非顺序块数据..." << std::endl;
+    pending_buffer.insert( std::make_pair( first_index, data ) );
+    _bytes_pending += data.length();
+    if ( first_index < first_unassembled_index ) {
+      first_unassembled_index = first_index;
     }
   }
-  // cout<<"``````"<<endl;
 }
 
 uint64_t Reassembler::bytes_pending() const
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -33,17 +33,12 @@ public:
   // How many bytes are stored in the Reassembler itself?
   uint64_t bytes_pending() const;
 
-protected:
-  std::map<uint64_t, std::string> pending_buffer;
-  /* Maintain LAS-LSR<=RWS */
-  uint64_t receive_window_size;
-  // uint64_t last_acceptable_bytes;
-  uint64_t last_bytes_received;
-  uint64_t last_ordered_bytes;
-  uint64_t _bytes_pending;
-  bool finish_flag;
-  uint64_t index_of_end;
-
-private:
-  void auto_push( uint64_t first_index, Writer& output, std::string current_data );
+  private:
+    std::map<uint64_t, std::string> pending_buffer;
+    uint64_t _bytes_pending;
+    // uint64_t first_unpoped_index;
+    uint64_t first_unassembled_index;
+    uint64_t _EOF_index;
+    bool received_FIN;
+    uint64_t available_capacity;
 };
