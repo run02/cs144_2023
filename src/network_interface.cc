6d8ea7b1f012ad85884a20e9b3fe6d3ff2434d34
Ryan <ryanlee02@foxmail.com>
2024-01-23 18:42:04 +0800

    这一章相对简单, 维护的状态只包括以下内容: 1) ARP缓存表<ip,mac> 2) 缓存的时间表<ip,last_reply_ms> 3) 没找到下一跳的ip数据报<ip,queue<ip_datagram> 4) 当前时间 5) 待发送的Ethernet的FIFO
    发ipv4数据报的时候, 如果缓存表中有ip的下一跳的mac地址, 且没过期, 就发
    如果过期了或者是没有在缓存表中找到, 就存起来. 然后发arp请求(同一ip5s内只发一次请求防止泛洪)
    
    收到ethernet数据帧的时候, 如果是给自己的ip数据帧, 就返回
    如果是Arp, 就在缓存表中更新发送方的arp缓存条目,
    如果还是询问自己的arp请求, 就回复
    如果是reply, 就把没找到下一跳的mac地址的那个ip对应的队列里的数据报清空出去.
    
    具体内容看check4即可.
    
    虽然通过了100%的测试用例, 但是如果局域网内出现了Arp泛洪, 在这里可能会导致缓存条目非常频繁的更新.
    如果有哪个设备发出一大堆不同的arp请求/回复, 而路由器又没有处理, 可能会让条目表变得巨大无比, 或者撑爆了...
    
    可惜check4中在faq中以下两种情况在本次lab中不考虑,
    i. ARP request but never gets a reply, send an ICMP “host unreachable” back
    ii. an InternetDatagram is queued waiting to learn the Ethernet address of the next hop, and that information never comes
    但是如果真要做起来到也比较容易, 只需额外记录一下进队列的时间, 然后定期检查超时的就丢掉即可

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -8,7 +8,14 @@ using namespace std;
 // ethernet_address: Ethernet (what ARP calls "hardware") address of the interface
 // ip_address: IP (what ARP calls "protocol") address of the interface
 NetworkInterface::NetworkInterface( const EthernetAddress& ethernet_address, const Address& ip_address )
-  : ethernet_address_( ethernet_address ), ip_address_( ip_address )
+  : ethernet_address_( ethernet_address )
+  , ip_address_( ip_address )
+  , ethernet_buffer()
+  , known_hosts()
+  , hosts_last_replay()
+  , arp_requests()
+  , current_time(0)
+  , unknown_hosts_ip_datagram_pending()
 {
   cerr << "DEBUG: Network interface has Ethernet address " << to_string( ethernet_address_ ) << " and IP address "
        << ip_address.ip() << "\n";
@@ -22,24 +29,72 @@ NetworkInterface::NetworkInterface( const EthernetAddress& ethernet_address, con
 // Address::ipv4_numeric() method.
 void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )
 {
-  (void)dgram;
-  (void)next_hop;
+  cout<<"NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )"<<endl;
+  uint32_t ip4=next_hop.ipv4_numeric();
+  auto it=known_hosts.find(ip4);
+  //如果找到了, 看看是否过期, 没过期, 就发送
+  if(it!=known_hosts.end()){
+    //超过5s没询问了
+    if(current_time -  hosts_last_replay[it->first] >= NetworkInterface::ARP_MAP_TIMEOUT){
+      //每5s只发一次, 防止arp请求泛洪
+      send_arp_request_frame(ip4);
+      pending_ip_datagram(dgram,ip4);
+    }else{
+      send_ipv4_frame(dgram,it->second);
+    }
+  }else{//, 找不到就先存着, 发Arp请求, 等找到了再发送
+    pending_ip_datagram(dgram,ip4);
+    send_arp_request_frame(ip4);
+  }
 }
 
 // frame: the incoming Ethernet frame
 optional<InternetDatagram> NetworkInterface::recv_frame( const EthernetFrame& frame )
 {
-  (void)frame;
+  cout<<"NetworkInterface::recv_frame( const EthernetFrame& frame )"<<endl;
+  //如果是ipv4的数据包就发给调用的, 如果是Arp就处理Aro请求
+  if(frame.header.type==EthernetHeader::TYPE_IPv4&&frame.header.dst==ethernet_address_){
+    InternetDatagram ipv4_frame;
+    if (parse(ipv4_frame,frame.payload)){
+       return ipv4_frame;
+    }
+  }else if(frame.header.type==EthernetHeader::TYPE_ARP){//Learn mappings from both requests and replies.
+    ARPMessage arp_message;
+    if (parse(arp_message,frame.payload)){
+      if(arp_message.opcode==ARPMessage::OPCODE_REPLY){
+        known_hosts[arp_message.sender_ip_address]=arp_message.sender_ethernet_address;
+        hosts_last_replay[arp_message.sender_ip_address]=current_time;
+        auto pending= unknown_hosts_ip_datagram_pending.find(arp_message.sender_ip_address);
+          if(pending!=unknown_hosts_ip_datagram_pending.end()){
+            while(!pending->second.empty()){
+              send_ipv4_frame(pending->second.front(),arp_message.sender_ethernet_address);
+              pending->second.pop();
+            }
+          }
+      }else if(arp_message.opcode==ARPMessage::OPCODE_REQUEST){//如果是询问本机的mac地址和ip, 就应答
+        known_hosts[arp_message.sender_ip_address]=arp_message.sender_ethernet_address;
+        hosts_last_replay[arp_message.sender_ip_address]=current_time;
+        if(arp_message.target_ip_address==ip_address_.ipv4_numeric()){
+          send_arp_reply_frame(arp_message);
+        }
+      }  
+    }
+  }
   return {};
 }
 
 // ms_since_last_tick: the number of milliseconds since the last call to this method
 void NetworkInterface::tick( const size_t ms_since_last_tick )
 {
-  (void)ms_since_last_tick;
+  current_time+=ms_since_last_tick;
 }
 
 optional<EthernetFrame> NetworkInterface::maybe_send()
 {
+  if(!ethernet_buffer.empty()){
+    auto ethernet_frame=ethernet_buffer.front();
+    ethernet_buffer.pop();
+    return ethernet_frame;
+  }
   return {};
 }
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -3,6 +3,7 @@
 #include "address.hh"
 #include "ethernet_frame.hh"
 #include "ipv4_datagram.hh"
+#include "arp_message.hh"
 
 #include <iostream>
 #include <list>
@@ -34,6 +35,8 @@
 // and learns or replies as necessary.
 class NetworkInterface
 {
+ const static uint ARP_MAP_TIMEOUT=30000;
+ const static uint ARP_REQUEST_INTERVAL=5000;
 private:
   // Ethernet (known as hardware, network-access, or link-layer) address of the interface
   EthernetAddress ethernet_address_;
@@ -41,6 +44,56 @@ private:
   // IP (known as Internet-layer or network-layer) address of the interface
   Address ip_address_;
 
+  std::queue<EthernetFrame> ethernet_buffer;
+  std::unordered_map<uint32_t,EthernetAddress> known_hosts;//记录<ip,mac> Learn mappings from both requests and replies.
+  std::unordered_map<uint32_t,uint64_t> hosts_last_replay; //<ip, last_time>记录ip上次应答的时间
+  std::unordered_map<uint32_t,uint64_t> arp_requests; //<ip,last_time>上次问的时间>记录上次发出arp请求询问该ip的时间, 如果5s内已经问过了, 就不要再问这个ip的mac了
+  uint64_t current_time;
+  std::unordered_map<uint32_t,std::queue<InternetDatagram>> unknown_hosts_ip_datagram_pending;
+  void send_ipv4_frame(IPv4Datagram dgram,EthernetAddress next_hop){
+    EthernetFrame eth_frame;
+    eth_frame.header.src=ethernet_address_;
+    eth_frame.header.dst=next_hop;
+    eth_frame.header.type=EthernetHeader::TYPE_IPv4;
+    eth_frame.payload=serialize(dgram);
+    ethernet_buffer.push(eth_frame);
+  }
+  void pending_ip_datagram(IPv4Datagram dgram,uint32_t ip4){
+    unknown_hosts_ip_datagram_pending[ip4].push(dgram);
+  }
+  void send_arp_request_frame(uint32_t ip4){
+    EthernetFrame eth_frame;
+    eth_frame.header.src=ethernet_address_;
+    auto arp_cache=arp_requests.find(ip4);
+    if((arp_cache!=arp_requests.end())&&(current_time - arp_cache->second<ARP_REQUEST_INTERVAL)){
+      return;
+    }
+    eth_frame.header.type=EthernetHeader::TYPE_ARP;
+    eth_frame.header.dst=ETHERNET_BROADCAST;
+    ARPMessage arp_msg;
+    arp_msg.sender_ip_address=ip_address_.ipv4_numeric();
+    arp_msg.sender_ethernet_address=ethernet_address_;
+    arp_msg.opcode=ARPMessage::OPCODE_REQUEST;
+    arp_msg.target_ip_address=ip4;
+    eth_frame.payload=serialize(arp_msg);
+    ethernet_buffer.push(eth_frame);
+    arp_requests[ip4]=current_time;
+  }
+  void send_arp_reply_frame(ARPMessage arp_message){
+    ARPMessage arp_msg;
+    arp_msg.sender_ip_address=ip_address_.ipv4_numeric();
+    arp_msg.sender_ethernet_address=ethernet_address_;
+    arp_msg.opcode=ARPMessage::OPCODE_REPLY;
+    arp_msg.target_ip_address=arp_message.sender_ip_address;
+    arp_msg.target_ethernet_address=arp_message.sender_ethernet_address;
+    EthernetFrame eth_frame;
+    eth_frame.header.type=EthernetHeader::TYPE_ARP;
+    eth_frame.header.src=ethernet_address_;
+    eth_frame.header.dst=arp_message.sender_ethernet_address;
+    eth_frame.payload=serialize(arp_msg);
+    ethernet_buffer.push(eth_frame); 
+  }
+  
 public:
   // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
   // addresses
